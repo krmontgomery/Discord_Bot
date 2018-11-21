b16aa75e7817c0b2ed8d6c9db69581dff00aef64
Kris Montgomery <kmontgomery91@gmail.com>
2018-11-21 12:39:23 -0600

    created new embed post skeleton

--- index.js
+++ index.js
@@ -196,10 +196,29 @@ module.exports.run = (client,message,args) => {
         .addField("Want more news? Wanna make this better for you and the Server?")
         .addField("Put your ideas in the Alt Ctrl Elite server!", "Any ideas you may have for a news command bot, put in the fupa bot channel in the Alt Ctrl Elite Server. Thanks!.", true)
 
-    if(message.content.includes(prefix + "news")){
-        message.channel.send({embed});
+const theNews = {embed:{
+    color:0x000AE86,
+    author: {
+        name: client.user.username,
+        icon_url: client.user.avatarURL,
+        fields:{
+            author: "Lauren Frayer",
+            text:"About Lauren:",
+            url:"https://www.npr.org/people/463861805/lauren-frayer"
+        },
+        description:"Holy Crap!",
+        url:"https://www.npr.org/2018/11/21/669909594/american-reportedly-killed-in-flurry-of-arrows-as-tribe-defends-its-island-off-i",
+        images:"https://media.npr.org/assets/img/2018/11/21/ap_050104019273_wide-cf719d3cda02d856a7adb9367793f25e8631030d-s1600-c85.jpg"
+        }, 
+        footer:{
+            icon_url: client.user.avatarURL,
+            text: "© Your Dankness"
+          }     
     }
-};
+}}
+if(message.content.includes(prefix + "news")){
+        message.channel.send(theNews);
+    }
 
 
 const token = process.env.token;
